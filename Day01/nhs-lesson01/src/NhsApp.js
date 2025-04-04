3b5936568a2f1a412f5a4c5ffefb611596dbe7a4
Nguyen Hai Son <sonnguyennr111@gmail.com>
2025-04-04 08:51:20 +0700

    Them thu muc Day01

---
+++ Day01/nhs-lesson01/src/NhsApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NhsApp() {
+  return (
+    <div className='container boder my-3'>
+      <h1>Nguyen Hai Son - Demo</h1>
+      <hr/>
+    </div>
+  )
+}
